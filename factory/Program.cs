353d14c3cafd0be7add0eb49a6020210fb173420
Krishna Sarigopula <krishna.sarigopula@trianz.com>
2022-09-14 16:40:23 +0530

    factory pattern

---
+++ factory/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IVechile vechile = Factory.GetVechile("car");
+            vechile.Write();
+            vechile = Factory.GetVechile("bike");
+            vechile.Write();
+        }
+    }
+
+    interface IVechile
+    {
+        void Write();
+    }
+
+    class Car : IVechile
+    {
+        public void Write()
+        {
+            Console.WriteLine("I am car");
+        }
+    }
+
+    class Bike : IVechile
+    {
+        public void Write()
+        {
+            Console.WriteLine("I am Bike");
+        }
+    }
+
+    class Factory
+    {
+        public static IVechile GetVechile(string type)
+        {
+            IVechile vechile = null;
+
+            if (type == "car")
+                vechile = new Car();
+            else if (type == "bike")
+                vechile = new Bike();
+
+            return vechile;
+        }
+    }
+}
